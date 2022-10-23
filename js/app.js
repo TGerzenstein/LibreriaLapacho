1794ccf8ba9f7de168a770b16e7b13048f701779
TGerzenstein <tatigerzens@gmail.com>
2022-10-23 19:31:28 -0300

    modified in app js

--- js/app.js
+++ js/app.js
@@ -4,37 +4,38 @@ const productList = [
     {id: 2, title: "Federico Garcia Lorca", precio: 3000},
     {id: 3, title: "Silvina Ocampo", precio: 1200}
 ]
-//agregar precio;
-//Variables a inglés; 
 
-var carrito = []
+let localStorageCart = localStorage.getItem('myapp_cart');
+if (localStorageCart) {
+    localStorageCart = JSON.parse(localStorageCart)
+}
 
-//document.querySelector("h2").textContent = "libro nuevo"
-//console.log(document.querySelector("h2"))
-
-//document.getElementById("parrafoNuevo").textContent = "La mayoría de los libros son buenos"
-//console.log(document.getElementById("parrafoNuevo"))
-
-const parrafoNuevo = document.querySelector("#parrafoNuevo")
-//parrafoNuevo.textContent = "Hola si"
-//parrafoNuevo.innerHTML = "<p>Resistencia - Chaco</p>"
+var carrito = localStorageCart || [];
 
 const addToCartBtn = Array.from(document.getElementsByClassName("add-to-cart"))
-console.log(addToCartBtn)
+console.log(addToCartBtn);
 addToCartBtn.forEach(element => {
     element.addEventListener("click", addToCart)
-
 });
+
 function addToCart(event) {
     event.preventDefault()
     let prodId = this.id.split("-")
     prodId = parseInt(prodId[1]); 
     console.log(prodId);
     seleccionarProducto(prodId);
-}
+};
+
+function initShop() {
+    let totalProductos = calcularTotalCarrito();
+    let impuestos = 12;
+    let gastosEnvio = 800;
+    let total = totalProductos + impuestos + gastosEnvio;
+    const carTotalelement = document.getElementById("cart-total");
+    carTotalelement.innerHTML = total;
+};
 
 function seleccionarProducto(libro) {
-//    let libro = parseInt(libro);
     let producto = encontrarProducto(libro);
     carrito.push(producto);
     let totalProductos = calcularTotalCarrito();
@@ -43,9 +44,9 @@ function seleccionarProducto(libro) {
     let total = totalProductos + impuestos + gastosEnvio;
     const carTotalelement = document.getElementById("cart-total");
     carTotalelement.innerHTML = total;
-
-    //seleccionarOperacion(total);
-}
+    updateOrder(total);
+    localStorage.setItem('myapp_cart', JSON.stringify(productList));
+};
 
 function calcularTotalCarrito() {
     let totalCarrito = 0;
@@ -53,15 +54,31 @@ function calcularTotalCarrito() {
             totalCarrito = producto.precio + totalCarrito 
     });   
         return totalCarrito; 
-}
+};
 
 function encontrarProducto(id) {
     const encontrado = productList.find((producto) => {
         return producto.id === id;
     });
     return encontrado;
-}
+};
 
+function updateOrder(total) {
+    let ladrillo = '<div class="shop-box1">' +
+    '<p>Un cuarto propio - Virginia Woolf</p>' +
+    '<p>$ 6000</p>' +
+  '</div>';
+    let output = '' 
+    carrito.forEach((producto) => {
+         output += '<div class="shop-box1">' +
+    '<p>'+ producto.title +'</p>' +
+    '<p>'+ producto.precio +'</p>' + 
+  '</div>';
+    })
+    output += '<p>'+ total +'</p>';
+    console.log(output);
+    let carTotalprod = document.getElementById("shop-box-list");
+    carTotalprod.innerHTML = output;
+};
 
-
-
+initShop()
